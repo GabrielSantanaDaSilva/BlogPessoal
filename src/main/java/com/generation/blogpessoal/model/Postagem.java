432e0cf1805d72b34204f5b772159ffc2439d57c
GabrielSantanaDaSilva <topsstdeq@gmail.com>
2024-07-02 14:25:59 -0300

    Primeira task do BLOG

---
+++ src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -0,0 +1,65 @@
+package com.generation.blogpessoal.model;
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import jakarta.persistence.Id;
+
+@Entity
+@Table(name = "tb_postagens")
+public class Postagem {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id_postagem;
+	
+	@NotBlank(message = "O titulo é obrigatório!!!")
+	@Size(min = 5, max = 100, message = "O titulo deve conter no minímo 5 e no máximo 100 caracteres!")
+	private String titulo;
+	
+	@NotBlank(message = "O texto é obrigatório")
+	@Size(min = 10, max = 1000, message = "o texto deve conter no míniimo 10 e no máximo 1000 caracteres")
+	private String texto;
+	
+	@UpdateTimestamp
+	private LocalDateTime data;
+
+	public long getId_postagem() {
+		return id_postagem;
+	}
+
+	public void setId_postagem(long id_postagem) {
+		this.id_postagem = id_postagem;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+	public LocalDateTime getData() {
+		return data;
+	}
+
+	public void setData(LocalDateTime data) {
+		this.data = data;
+	}
+	
+
+}
